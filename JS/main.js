40109b41b04890c008db5726f63794df886ee792
felixfaisal <faisalahmedfarooq46@gmail.com>
2019-03-20 00:04:28 +0530

    Working on adding and removing nodes

--- JS/main.js
+++ JS/main.js
@@ -5,14 +5,33 @@
   var k = document.getElementById('id');
 }*/
 var f=[];
+var l=[];
 function process() {
   'use strict'
   //document.getElementById('final').innerHTML='<p>Hello</p>';
+  document.getElementById('myform').style.visibility='hidden';
+  document.getElementById('newbtn').style.visibility='hidden';
+  document.getElementById('btns').style.visibility='visible';
   var k=document.getElementById('name').value;
   var n=document.getElementById('number').value;
   f.push(k);
-  document.getElementById('final').innerHTML='<p>'+f[0]+'</p>';
+  l.push(n);
+
+  for(let i=f.length-1;i>=0;i--){
+
+  //var h='<p>'+f[i]+'</p><br>';
+    var table = document.createElement('table');
+    var row = document.createElement('tr');
+    var data = document.createElement('td');
+    var newtext = document.createTextNode(f[i]);
+    table.appendChild(row);
+    table.appendChild(data);
+    table.appendChild(newtext);
+  //document.getElementById('final').innerHTML='<p>'+f[i]+'</p>';
   //document.getElementById('final').innerHTML='<output name="x" for="name"></output>';
+    var p1 = document.getElementById("final");
+    p1.appendChild(table);
+  }
 }
 function show() {
   'use strict'
